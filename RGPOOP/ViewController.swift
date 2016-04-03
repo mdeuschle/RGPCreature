a6bc630ade10387d21417df2ceba00d8b40b2c69
Matt Deuschle <mdeuschle@gmail.com>
2016-04-03 09:49:43 -0700

    completed storyboard

--- RGPOOP/ViewController.swift
+++ RGPOOP/ViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var printLabel: UILabel!
+    @IBOutlet var playerHpLabel: UILabel!
+    @IBOutlet var enemyHpLabel: NSLayoutConstraint!
+    @IBOutlet var enemyImage: UIImageView!
+    @IBOutlet var chestButton: UIButton!
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    @IBAction func onChestTapped(sender: AnyObject) {
+    }
 }
 
